d9bb2a2308f826d21dd9dae385fe8727a2c0cd77
lucioacri <lucioacri@gmail.com>
2025-04-10 15:36:07 +0200

    add dropdown

--- js/script.js
+++ js/script.js
@@ -4,12 +4,19 @@ const sendButton = document.getElementById("send-button");
 const deleteButton = document.getElementById("delete-button");
 const ticketChoiceForm = document.getElementById("ticket-choice");
 const errorAlert = document.getElementById("error-message");
+const ages = document.getElementById("inputGroupSelect01");
 const kmPrice = 0.21;
+let finalPrice = 0;
+let discountedPrice = 0;
+
+console.log(ages);
 
 ticketChoiceForm.addEventListener("submit", function (event) {
   event.preventDefault();
   const fullName = userCredentials.value;
   const fixedDistance = parseInt(kmDistance.value);
+  const agesRange = ages.option.value;
+  console.log(agesRange);
 
   let correctInputs = false;
   if (!fullName || !isNaN(fullName)) correctInputs = true;
@@ -17,30 +24,23 @@ ticketChoiceForm.addEventListener("submit", function (event) {
 
   if (correctInputs) {
     errorAlert.classList.remove("d-none");
-  } else errorAlert.classList.add("d-none");
+    return;
+  }
 
+  errorAlert.classList.add("d-none");
+  if (agesRange === "Minorenne") {
+    const discount20 = (finalPrice * 20) / 100;
+    discountedPrice = finalPrice - discount20;
+  }
+  if (agesRange === "Over 65") {
+    const discount40 = (finalPrice * 40) / 100;
+    discountedPrice = finalPrice - discount40;
+  } else {
+    finalPrice = fixedDistance * kmPrice;
+  }
+
+  console.log(finalPrice);
   console.log(fullName);
   console.log(fixedDistance);
+  console.log(discountedPrice);
 });
-
-// let outputText;
-
-// if (isNaN(distance)) {
-//   alert("Distanza non corretta");
-// } else {
-//   const ages =
-
-//     const price = distance * kmPrice;
-//     console.log("price", price);
-//     if (ages === "Minorenne") {
-//       const discount20 = (price * 20) / 100;
-//       outputText = price - discount20;
-//     }
-//     if (ages === "Over 65") {
-//       const discount40 = (price * 40) / 100;
-//       outputText = price - discount40;
-//     } else {
-//       outputText = price;
-//     }
-
-// }
