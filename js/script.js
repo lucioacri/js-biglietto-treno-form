b97c76db998cee8580315f7a75a3eda2c513922a
lucioacri <lucioacri@gmail.com>
2025-04-10 16:10:27 +0200

    add calculations

--- js/script.js
+++ js/script.js
@@ -7,20 +7,21 @@ const errorAlert = document.getElementById("error-message");
 const ages = document.getElementById("inputGroupSelect01");
 const kmPrice = 0.21;
 let finalPrice = 0;
-let discountedPrice;
+let discountedPrice = 0;
 
-console.log(ages);
+// console.log(ages);
 
 ticketChoiceForm.addEventListener("submit", function (event) {
   event.preventDefault();
   const fullName = userCredentials.value;
   const fixedDistance = parseInt(kmDistance.value);
   const agesRange = ages.value;
-  console.log(agesRange);
+  console.log("range di età", agesRange);
 
   let correctInputs = false;
   if (!fullName || !isNaN(fullName)) correctInputs = true;
-  if (!fixedDistance || isNaN(fixedDistance)) correctInputs = true;
+  if (!fixedDistance || isNaN(fixedDistance) || fixedDistance < 0)
+    correctInputs = true;
 
   if (correctInputs) {
     errorAlert.classList.remove("d-none");
@@ -30,17 +31,19 @@ ticketChoiceForm.addEventListener("submit", function (event) {
   errorAlert.classList.add("d-none");
   if (agesRange === "1") {
     const discount20 = (finalPrice * 20) / 100;
-    discountedPrice = finalPrice - discount20;
+    discountedPrice += finalPrice - discount20;
+    console.log("prezzo scontato 20", discountedPrice);
   }
   if (agesRange === "3") {
     const discount40 = (finalPrice * 40) / 100;
     discountedPrice = finalPrice - discount40;
-  } else {
+    console.log("prezzo scontato 40", discountedPrice);
+  }
+  if (agesRange === "2") {
     finalPrice = fixedDistance * kmPrice;
   }
 
-  console.log(finalPrice);
-  console.log(fullName);
-  console.log(fixedDistance);
-  console.log(discountedPrice);
+  console.log("prezzo finale", finalPrice);
+  console.log("nome e cognome", fullName);
+  console.log("distanza", fixedDistance);
 });
