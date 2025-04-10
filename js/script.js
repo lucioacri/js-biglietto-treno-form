296cf561502fe9647ae1ba1997f4df90b535862f
lucioacri <lucioacri@gmail.com>
2025-04-10 18:44:22 +0200

    add some css and html

--- js/script.js
+++ js/script.js
@@ -6,9 +6,9 @@ const ticketChoiceForm = document.getElementById("ticket-choice");
 const errorAlert = document.getElementById("error-message");
 const ages = document.getElementById("inputGroupSelect01");
 const kmPrice = 0.21;
-let finalPrice = 0;
 
 // CARDS
+const hiddenCard = document.getElementById("hidden-card");
 const cardUserName = document.querySelector("#name-card .card-user");
 const cardRange = document.querySelector("#range-card .card-range");
 const cardPrice = document.querySelector("#price-card .card-price");
@@ -20,39 +20,44 @@ ticketChoiceForm.addEventListener("submit", function (event) {
   const fullName = userCredentials.value;
   const fixedDistance = parseInt(kmDistance.value);
   let agesRange = ages.value;
+  let ageForCard = "Maggiorenne";
   console.log("range di età", agesRange);
 
   let correctInputs = false;
-  if (!fullName || !isNaN(fullName)) correctInputs = true;
-  if (!fixedDistance || isNaN(fixedDistance) || fixedDistance < 0)
+  if (!fullName || !isNaN(fullName)) {
     correctInputs = true;
-
+  }
+  if (!fixedDistance || isNaN(fixedDistance) || fixedDistance < 0) {
+    correctInputs = true;
+  }
   if (correctInputs) {
     errorAlert.classList.remove("d-none");
-    return;
-  }
+    hiddenCard.classList.replace("d-block", "d-none");
+  } else {
+    errorAlert.classList.add("d-none");
+    hiddenCard.classList.replace("d-none", "d-block");
+    let finalPrice = fixedDistance * kmPrice;
+    if (agesRange === "minorenne") {
+      ageForCard = "Minorenne";
+      const discount20 = (finalPrice * 20) / 100;
+      finalPrice = fixedDistance * kmPrice - discount20;
+    }
+    if (agesRange === "over-65") {
+      ageForCard = "Over 65";
+      const discount40 = (finalPrice * 40) / 100;
+      finalPrice = fixedDistance * kmPrice - discount40;
+    }
+    if (agesRange === "maggiorenne") {
+      ageForCard = "Maggiorenne";
+      finalPrice = fixedDistance * kmPrice;
+    }
+    hiddenCard.classList.replace("d-none", "d-flex");
 
-  errorAlert.classList.add("d-none");
-  if (agesRange === "1") {
-    agesRange = "Minorenne";
-    const discount20 = (finalPrice * 20) / 100;
-    finalPrice = fixedDistance * kmPrice - discount20;
+    cardUserName.innerText = `${fullName}`;
+    cardRange.innerText = `${ageForCard}`;
+    cardPrice.innerText = `${finalPrice}`;
   }
-  if (agesRange === "3") {
-    agesRange = "Over 65";
-    const discount40 = (finalPrice * 40) / 100;
-    finalPrice = fixedDistance * kmPrice - discount40;
-  }
-  if (agesRange === "2") {
-    agesRange = "Maggiorenne";
-    finalPrice = fixedDistance * kmPrice;
-  }
-
-  console.log("prezzo finale", finalPrice);
-  console.log("nome e cognome", fullName);
-  console.log("distanza", fixedDistance);
-
-  cardUserName.innerText = `${fullName}`;
-  cardRange.innerText = `${agesRange}`;
-  cardPrice.innerText = `${finalPrice}`;
+  // if (!correctInputs) {
+  // } else {
+  // }
 });
